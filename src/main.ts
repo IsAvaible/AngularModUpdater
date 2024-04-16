aaf1b416f2bec535decd8d59e56660d4d06e7da8
Simon Conrad <simon.conrad@alumni.fh-aachen.de>
2024-04-16 14:25:36 +0200

    Add speed insights powered by [Vercel](https://vercel.com/docs/speed-insights)

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 inject();
+injectSpeedInsights();
