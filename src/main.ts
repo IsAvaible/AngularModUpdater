ba4f76d45ba9224e08e628d46227e7fc9ad22845
Simon Conrad <simon.felix.conrad@proton.me>
2026-02-10 13:32:45 +0100

    fix(deployment): disable Vercel speed insights

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { inject } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
+// import { injectSpeedInsights } from '@vercel/speed-insights';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
 
 inject();
-injectSpeedInsights();
+// Speed Insights are currently disabled as they triggered quota limits pausing the deployment.
+// injectSpeedInsights();
